ae1f7546642d7d7cbafd0add9c7c6c55382d399e
elif <elifsancak252@gmail.com>
2019-10-10 12:43:55 +0200

    sort js edited

--- week-1-project/sort-handler.js
+++ week-1-project/sort-handler.js
@@ -18,8 +18,12 @@ function sortHandler() {
   const toSort = document.getElementById('sort-input').value;
 
   // pass user input through core logic (write this! it doesn't work)
-  const sorted = `sort the charecters in ${toSort}`;
-
+  //const sorted = `sort the charecters in ${toSort}`;
+function sirala(word){
+let yenidensirala = word.split('');
+let yenisiralama = yenidensirala.sort();
+return yenisiralama.join('');
+}
   // report result to user (this works, no need to change it!)
   const outputField = document.getElementById('sort-output');
   outputField.innerHTML = sorted;
